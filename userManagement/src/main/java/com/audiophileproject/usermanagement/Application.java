30e6604de4a2b99de7ce1f130fe5374be1acfcc4
mehdi ouledhali <mehdi.ouledhali@gmail.com>
2023-02-19 17:06:32 +0100

    install sring boot dependency and bootstrap the app

---
+++ userManagement/src/main/java/com/audiophileproject/usermanagement/Application.java
@@ -0,0 +1,12 @@
+package com.audiophileproject.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        // System.out.println("Hello world!");
+        SpringApplication.run(Application.class, args);
+    }
+}
