71bf69abeac848c01ae798b11fb729d6556e75ba
mehdi ouledhali <mehdi.ouledhali@gmail.com>
2023-02-19 18:41:07 +0100

    create user class

---
+++ userManagement/src/main/java/com/audiophileproject/usermanagement/User.java
@@ -0,0 +1,14 @@
+package com.audiophileproject.usermanagement;
+
+
+
+public class User {
+    
+    private Integer id;
+    private String email;
+    private String username;
+    private String password;
+
+
+
+}
