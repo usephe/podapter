0efbb678a68836c03816ef56dfbb357ec3fe65de
mehdi ouledhali <mehdi.ouledhali@gmail.com>
2023-02-20 18:34:20 +0100

    update to jakarta and add setters and getter

--- userManagement/src/main/java/com/audiophileproject/usermanagement/User.java
+++ userManagement/src/main/java/com/audiophileproject/usermanagement/User.java
@@ -1,14 +1,83 @@
 package com.audiophileproject.usermanagement;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
-
+@Entity
+@Table
 public class User {
     
+    @Id
+    @SequenceGenerator(
+        name = "user_sequence",
+        sequenceName= "user_sequence",
+        allocationSize=1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "user_sequence"
+    )
     private Integer id;
     private String email;
     private String username;
     private String password;
 
+    public User(){
 
+    }
+
+    public User(Integer id, String email, String username, String password) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", email=" + email + ", username=" + username + ", password=" + password + "]";
+    }
 
 }
