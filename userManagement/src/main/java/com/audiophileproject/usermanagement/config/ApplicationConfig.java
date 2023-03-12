8cdb1184ce5d385e130be72cadf08a219356bef0
mehdi ouledhali <mehdi.ouledhali@gmail.com>
2023-03-12 15:02:38 +0100

    implement the UserDetailsService in its own UserService

--- userManagement/src/main/java/com/audiophileproject/usermanagement/config/ApplicationConfig.java
+++ userManagement/src/main/java/com/audiophileproject/usermanagement/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package com.audiophileproject.usermanagement.config;
 
 import com.audiophileproject.usermanagement.repos.UserRepository;
+import com.audiophileproject.usermanagement.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +9,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,16 +16,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class ApplicationConfig {
     private  final UserRepository userRepository;
-    @Bean
-    public UserDetailsService userDetailsService(){
-        return  username -> userRepository.findByEmail(username)
-                .orElseThrow(()-> new UsernameNotFoundException("User Not Found !"));
-    }
-
+    private final UserService userDetailsService;
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
--- userManagement/src/main/java/com/audiophileproject/usermanagement/services/UserService.java
+++ userManagement/src/main/java/com/audiophileproject/usermanagement/services/UserService.java
@@ -1,9 +1,17 @@
 package com.audiophileproject.usermanagement.services;
 
+import com.audiophileproject.usermanagement.models.User;
 import com.audiophileproject.usermanagement.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class UserService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements org.springframework.security.core.userdetails.UserDetailsService {
 
     private  final UserRepository userRepository;
 
@@ -12,4 +20,13 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return  userRepository.findByEmail(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User Not Found !"));
+    }
 }
