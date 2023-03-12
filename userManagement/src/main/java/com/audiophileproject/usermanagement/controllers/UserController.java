cbe5d214b84a06fd592397aaf7e418a27a62291f
mehdi ouledhali <mehdi.ouledhali@gmail.com>
2023-03-12 23:00:30 +0100

    add update user request endpoint

--- userManagement/src/main/java/com/audiophileproject/usermanagement/controllers/UserController.java
+++ userManagement/src/main/java/com/audiophileproject/usermanagement/controllers/UserController.java
@@ -2,15 +2,20 @@ package com.audiophileproject.usermanagement.controllers;
 
 import com.audiophileproject.usermanagement.models.User;
 import com.audiophileproject.usermanagement.models.UserResponse;
+import com.audiophileproject.usermanagement.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
     @GetMapping
     public UserResponse getCurrentUser(){
      User currUser = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -21,5 +26,21 @@ public class UserController {
              .lastname(currUser.getLastname())
              .lastname(currUser.getLastname())
              .build();
+    }
+
+    @PutMapping
+    public UserResponse updateUser(@RequestBody User user){
+        User newUser = userService.updateUser(user).orElseThrow(()->new UsernameNotFoundException("User Not Found"));
+
+        return UserResponse.builder()
+                .email(newUser.getEmail())
+                .firstname(newUser.getFirstname())
+                .lastname(newUser.getLastname())
+                .build();
+    }
+    @DeleteMapping("/{username}")
+    public String deleteUser(@RequestParam String username){
+        userService.deleteUser(username);
+        return "user deleted successfully";
     }
 }
