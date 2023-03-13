505c244d7c613efc4a5b6675935472656e4008d5
mehdi ouledhali <mehdi.ouledhali@gmail.com>
2023-03-13 01:38:54 +0100

    add refresh method to the auth service... but it still not completed yet

--- userManagement/src/main/java/com/audiophileproject/usermanagement/services/AuthenticationService.java
+++ userManagement/src/main/java/com/audiophileproject/usermanagement/services/AuthenticationService.java
@@ -2,10 +2,15 @@ package com.audiophileproject.usermanagement.services;
 
 import com.audiophileproject.usermanagement.models.*;
 import com.audiophileproject.usermanagement.repos.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +21,7 @@ public class AuthenticationService {
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
 
 
     // TODO: validate the data before saving it
@@ -59,4 +65,41 @@ public class AuthenticationService {
                 .build();
     }
 
+
+    /**
+     * This method will be responsible for validating refresh token, and if valid, return a new jwt token
+     * @param request
+     * @return AuthenticationResponse
+     *
+     */
+    // TODO complete this method
+    public AuthenticationResponse refresh(HttpServletRequest request) {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+//            filterChain.doFilter(request,response);
+//            return;
+        }
+
+        jwt = authHeader.substring(7);
+        username = jwtService.extractUsername(jwt);
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            if(jwtService.isTokenValid(jwt,userDetails)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        return null;
+    }
 }
