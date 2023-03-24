946ad1e31f0af4cdd468ad2e2af3437b845931df
Useph El <el@useph.me>
2023-03-24 16:06:21 +0000

    feat(contentManagement): discover services via a discovery server
    Using a discovery server instead of hard coding the other services URLs

--- contentManagement/src/main/java/com/audiophileproject/main/config/AppConfig.java
+++ contentManagement/src/main/java/com/audiophileproject/main/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.audiophileproject.main.config;
 
 import com.audiophileproject.main.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableDiscoveryClient
 public class AppConfig {
 	private final UserRepository userRepository;
 
