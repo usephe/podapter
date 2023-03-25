cc6c274d4a55da1faa713a0388163aad3e40d270
Useph El <el@useph.me>
2023-03-25 23:07:36 +0000

    feat(apigatewayService): adding the logic of validating the JWT

---
+++ api-gateway/src/main/java/main/services/JwtService.java
@@ -0,0 +1,45 @@
+package main.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+@Service
+public class JwtService {
+    private final JwtParser jwtParser;
+
+    public boolean isTokenValid(String token){
+        System.out.println("token = " + token);
+        return true;
+        // TODO: validate the token with the actual public key
+//        try {
+//            final var claims = extractClaims(token);
+//            return !isTokenExpired(token);
+//        } catch (Exception e) {
+//            return false;
+//        }
+    }
+    private Claims extractClaims(String token){
+        return jwtParser
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date EXtractExpirationDate(String token) {
+        return  extractClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return  EXtractExpirationDate(token).before(new Date());
+    }
+}
