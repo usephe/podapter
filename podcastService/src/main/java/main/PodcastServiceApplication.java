d986b5c27d20782b5064973f81a4868ca10dbb12
Useph El <el@useph.me>
2023-03-30 14:45:44 +0000

    feat(podcastService): initial microservice boilerplate

---
+++ podcastService/src/main/java/main/PodcastServiceApplication.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PodcastServiceApplication.class, args);
+    }
+}
