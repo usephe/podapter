0c864cf9be7c6a73a99cbf6dc882ef30a19274d1
Useph El <el@useph.me>
2023-04-24 21:19:31 +0100

    rename(podcastService): to podcastFeedGenerator
    Change the service name to mean more or less what the service do

--- podcastFeedGenerator/src/main/java/main/PodcastFeedGeneratorApplication.java
+++ podcastFeedGenerator/src/main/java/main/PodcastFeedGeneratorApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PodcastServiceApplication {
+public class PodcastFeedGeneratorApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PodcastServiceApplication.class, args);
+        SpringApplication.run(PodcastFeedGeneratorApplication.class, args);
     }
 }
