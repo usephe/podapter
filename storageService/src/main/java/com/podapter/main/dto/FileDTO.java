9b68e742992511ec4418567aa22972896eff5345
Useph El <el@useph.me>
2023-04-28 18:06:43 +0100

    feat(storageService): expose the download URL via the response

--- storageService/src/main/java/com/podapter/main/dto/FileDTO.java
+++ storageService/src/main/java/com/podapter/main/dto/FileDTO.java
@@ -3,12 +3,15 @@ package com.podapter.main.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
+import java.net.URL;
+
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record FileDTO(
         Long id,
         String fileName,
         Long fileSize,
-        String fileType
+        String fileType,
+        URL url
 ) {
 }
