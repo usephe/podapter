5c5efd7226061e7fc2a4e075fb5310bf116098f3
Useph El <el@useph.me>
2023-05-04 20:44:33 +0100

    fix(apiGatewayService): hide the private endpoint

--- userManagement/src/main/java/com/audiophileproject/usermanagement/config/SecurityConfiguration.java
+++ userManagement/src/main/java/com/audiophileproject/usermanagement/config/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
+                .requestMatchers("/api/v1/auth/**", "/api/v1/user/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
